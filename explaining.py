cb9406858655173e1b86ffd58043317dee8e6e77
Oscar <ocrampton@kellettschool.com>
2024-09-30 07:04:28 +0000

    hi

---
+++ explaining.py
@@ -0,0 +1,9 @@
+for y in range(2):
+    print("giraffe")
+    for i in range(3):
+        print("fish")
+    for j in range(2):
+        print("cow")
+        for x in range(2):
+            print("chipmunk")
+            print("dog")
