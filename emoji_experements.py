d12e709b7741d53d0010618d1519c7cd82f4afd8
Oscar <ocrampton@kellettschool.com>
2024-10-07 07:11:31 +0000

    emoji

---
+++ emoji_experements.py
@@ -0,0 +1,17 @@
+sleepy = "hi"
+
+for i in range(10):
+    print(i,i * sleepy)
+
+
+bob = "bye"
+
+for i in range(10):
+    print(i * bob)
+
+for i in range(3, 31, 3):
+    print (i, i * sleepy)
+for i in range(1, 5):
+    print (i * sleepy)
+
+
